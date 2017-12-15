5b025d3d20f19fb37fd9305955bf6ea5ce97f0cb
王洁 <Jieremy@dhcp-v044-225.mobile.uci.edu>
2017-12-15 15:20:21 -0800

    dd

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
   var timeData = [],
     temperatureData = [],
-    humidityData = [],
-    timeData1 = [],
-    vibData=[];
- /* var data = {
+    humidityData = [];
+  var data = {
     labels: timeData,
     datasets: [
       {
@@ -30,24 +28,8 @@ $(document).ready(function () {
         data: humidityData
       }
     ]
-  }*/
-  var data1={
-    labels: timeData,
-    datasets: [
-      {
-        fill: false,
-        label: 'Vibration',
-        yAxisID: 'Vibration',
-        borderColor: "rgba(255, 204, 0, 1)",
-        pointBoarderColor: "rgba(255, 204, 0, 1)",
-        backgroundColor: "rgba(255, 204, 0, 0.4)",
-        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: vibData
-      }
-    ]
   }
-/*
+
   var basicOption = {
     title: {
       display: true,
@@ -73,24 +55,6 @@ $(document).ready(function () {
           position: 'right'
         }]
     }
-  }*/
-  var basicOption1 = {
-    title: {
-      display: true,
-      text: 'Vibration Real-time Data',
-      fontSize: 36
-    },
-    scales: {
-      yAxes: [{
-        id: 'Vibration',
-        type: 'linear',
-        scaleLabel: {
-          labelString: 'Vibration',
-          display: true
-        },
-        position: 'left',
-      }]
-    }
   }
 
   //Get the context of the canvas element we want to select
@@ -98,8 +62,8 @@ $(document).ready(function () {
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
-    data: data1,
-    options: basicOption1
+    data: data,
+    options: basicOption
   });
 
   var ws = new WebSocket('wss://' + location.host);
@@ -110,58 +74,29 @@ $(document).ready(function () {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
-      if(!obj.time || !obj.vib) {
+      if(!obj.time || !obj.temperature) {
         return;
       }
       timeData.push(obj.time);
-      vibData.push(obj.vib);
+      temperatureData.push(obj.temperature);
       // only keep no more than 50 points in the line chart
       const maxLen = 50;
       var len = timeData.length;
       if (len > maxLen) {
         timeData.shift();
-        vibData.shift();
+        temperatureData.shift();
       }
 
-      
+      if (obj.humidity) {
+        humidityData.push(obj.humidity);
+      }
+      if (humidityData.length > maxLen) {
+        humidityData.shift();
+      }
+
       myLineChart.update();
     } catch (err) {
       console.error(err);
     }
   }
- 
- /* var ctx1 = document.getElementById("myChart1").getContext("2d");
-  var optionsNoAnimation1= { animation: false }
-  var myLineChart1 = new Chart(ctx1, {
-    type: 'line',
-    data: data1,
-    options: basicOption1
-  });
-
-  var ws1 = new WebSocket('wss://' + location.host);
-  ws1.onopen = function () {
-    console.log('Successfully connect WebSocket');
-  }
-  ws1.onmessage = function (message) {
-    console.log('receive message' + message.data);
-    try {
-      var obj1 = JSON.parse(message.data);
-      if(!obj1.time || !obj1.gas) {
-        return;
-      }
-      timeData1.push(obj1.time);
-      gasData.push(obj1.gas);
-      // only keep no more than 50 points in the line chart
-      const maxLen1 = 50;
-      var len1 = timeData1.length;
-      if (len1 > maxLen1) {
-        timeData1.shift();
-        gasData.shift();
-      }
-
-      myLineChart1.update();
-    } catch (err) {
-      console.error(err);
-    }
-  }*/
 });
